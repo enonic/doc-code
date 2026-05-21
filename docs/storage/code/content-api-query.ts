662ab65dee4968621c162429f52af6d897074ca2
Thomas Sigdestad <tls@enonic.com>
2026-05-21 09:20:14 +0200

    introduced storage section + minor improvements

---
+++ docs/storage/code/content-api-query.ts
@@ -0,0 +1,26 @@
+import { query } from '/lib/xp/content';
+
+const result = query({
+    start: 0,
+    count: 2,
+    query: "data.city = 'Oslo' AND fulltext('data.description', 'garden', 'AND')",
+    filters: {
+        boolean: {
+            must: {
+                exists: { field: 'modifiedTime' },
+            },
+            mustNot: {
+                hasValue: {
+                    field: 'myField',
+                    values: ['cheese', 'fish', 'onion'],
+                },
+            },
+        },
+        notExists: {
+            field: 'unwantedField',
+        },
+        ids: {
+            values: ['id1', 'id2'],
+        },
+    },
+});
